52de7025ff57208c8fc8e6d77522ba7a16b3dcf9
JCarri14 <joancarrion14@gmail.com>
2019-05-03 21:29:21 +0200

    #5 Feat: Profile Interface v2
    Made some changes to the UI.

--- app/src/main/java/com/example/androidsocialnetwork/ProfileActivity.java
+++ app/src/main/java/com/example/androidsocialnetwork/ProfileActivity.java
@@ -2,12 +2,28 @@ package com.example.androidsocialnetwork;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView userDescription;
+    private TextView userHeight;
+    private TextView userBirthDate;
+    private TextView userGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userBirthDate = (TextView) findViewById(R.id.birthDate);
+        userHeight = (TextView) findViewById(R.id.heightValue);
+        userGender = (TextView) findViewById(R.id.genderValue);
+        userDescription = (TextView) findViewById(R.id.description);
+
+
+
     }
 }
