ae313708d15be39656a46bd72326faa4bc880689
JCarri14 <joancarrion14@gmail.com>
2019-05-04 21:22:36 +0200

    #5 Feat: Added Button attribute in Activity

--- app/src/main/java/com/example/androidsocialnetwork/ProfileActivity.java
+++ app/src/main/java/com/example/androidsocialnetwork/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.androidsocialnetwork;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -12,6 +13,7 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView userHeight;
     private TextView userBirthDate;
     private TextView userGender;
+    private Button btnEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,7 @@ public class ProfileActivity extends AppCompatActivity {
         userHeight = (TextView) findViewById(R.id.heightValue);
         userGender = (TextView) findViewById(R.id.genderValue);
         userDescription = (TextView) findViewById(R.id.description);
+        btnEdit = (Button) findViewById(R.id.btn_edit);
 
 
 
