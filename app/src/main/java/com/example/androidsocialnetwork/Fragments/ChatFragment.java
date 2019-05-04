d3b313a9341214153050f7c7ea83543ec60b0d4d
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-05-04 18:09:15 +0200

    #7 feat: Created Fragment

--- app/src/main/java/com/example/androidsocialnetwork/Fragments/ChatFragment.java
+++ app/src/main/java/com/example/androidsocialnetwork/Fragments/ChatFragment.java
@@ -1,7 +1,11 @@
 package com.example.androidsocialnetwork.Fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 public class ChatFragment extends Fragment {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
