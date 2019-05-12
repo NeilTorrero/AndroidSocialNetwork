0269914a4289e46bf20c8789ef7f73f405980cae
joseproig <josep.roig@students.salle.url.edu>
2019-05-12 16:58:59 +0200

    #13 feat: Now, when the video finalize, it changes the activity

--- app/src/main/java/com/example/androidsocialnetwork/SplashVideo.java
+++ app/src/main/java/com/example/androidsocialnetwork/SplashVideo.java
@@ -1,11 +1,14 @@
 package com.example.androidsocialnetwork;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.VideoView;
 
-public class SplashVideo extends AppCompatActivity {
+public class SplashVideo extends Activity {
     private VideoView startVideoApp;
 
     @Override
@@ -14,6 +17,14 @@ public class SplashVideo extends AppCompatActivity {
         setContentView(R.layout.splash_screen);
 
         startVideoApp = (VideoView) findViewById(R.id.v5prc);
+
+        startVideoApp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Intent intent = new Intent(SplashVideo.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         String vPath = "android.resource://com.example.androidsocialnetwork/" + R.raw.android_splashscreen;
         Uri uriPath = Uri.parse(vPath);
