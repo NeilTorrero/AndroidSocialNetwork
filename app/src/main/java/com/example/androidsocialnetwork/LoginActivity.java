4f2c5213728f98a624b6d2a9f4abf27671c927e3
joseproig <josep.roig@students.salle.url.edu>
2019-05-12 18:35:08 +0200

    #13 feat: Initialized the diffent controllers of the login activity
    But, I need one function to check the user existence with Retrofit.

--- app/src/main/java/com/example/androidsocialnetwork/LoginActivity.java
+++ app/src/main/java/com/example/androidsocialnetwork/LoginActivity.java
@@ -1,12 +1,39 @@
 package com.example.androidsocialnetwork;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+
+        final EditText emailUser = (EditText) findViewById(R.id.username_text_lg);
+        final EditText passwordUser = (EditText) findViewById(R.id.password_text_lg);
+        CheckBox checkBox = (CheckBox) findViewById(R.id.remember_me);
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkUserExistence(emailUser.getText().toString(),passwordUser.getText().toString())) {
+                    //We go to main activity
+                    Intent i = new Intent (getBaseContext(),MainActivity.class);
+                    startActivity(i);
+                }
+                //If the user no exists we stay in the same activity
+            }
+        });
+    }
+
+    public boolean checkUserExistence (String email, String password) {
+        //TODO: Retrofit have to tell me if the user exists in the platform
+        return true;
     }
 }
